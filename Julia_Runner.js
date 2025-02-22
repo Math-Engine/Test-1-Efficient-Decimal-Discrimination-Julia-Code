65ff8e812eec43809a9f8915749477c590c3b44a
happy-spark <107300540+happy-spark@users.noreply.github.com>
2025-02-22 15:48:47 +0900

    Create Julia_Runner.js

---
+++ Julia_Runner.js
@@ -0,0 +1,20 @@
+const { spawn } = require('child_process');
+
+const startTime = process.hrtime();
+
+const juliaProcess = spawn('julia', [${process.argv[2]}]);
+
+juliaProcess.stdout.on('data', (data) => {
+  process.stdout.write(data);
+});
+
+juliaProcess.stderr.on('data', (data) => {
+  process.stderr.write(data);
+});
+
+juliaProcess.on('close', (code) => {
+  const diff = process.hrtime(startTime);
+  const timeInSeconds = diff[0] + diff[1] / 1e9;
+  console.log(`${process.argv[2]} : ${timeInSeconds.toFixed(3)} seconds`);
+  execSync(`echo "${process.argv[2]} : ${timeInSeconds.toFixed(3)} seconds <br>" >> $GITHUB_STEP_SUMMARY`);
+});

8009ac7e18e8c6392c82af50eb0549919749a9bf
happy-spark <107300540+happy-spark@users.noreply.github.com>
2025-02-22 15:31:45 +0900

    Create Decimal_Discrimination-3.jl

---
+++ Decimal_Discrimination_Codes/Decimal_Discrimination-3.jl
@@ -0,0 +1,20 @@
+function is_prime(n::Int)
+    if n <= 1
+        return false
+    elseif n <= 3
+        return true
+    end
+    if (n - 1) % 6 != 0
+      if (n + 1) % 6 != 0
+        return false
+      end
+    end
+    i = 5
+    while i * i <= n
+        if n % i == 0 || n % (i + 2) == 0
+            return false
+        end
+        i = i + 6
+    end
+    return true
+end
